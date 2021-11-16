242b3ec1f697999cdf6d1cf4dc5d903cb8b11287
Yekaterina Bulekova <tushka2030@mail.ru>
2021-11-16 20:40:05 +0600

    added new solution

---
+++ ConsoleApptest1/ConsoleApptest1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApptest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
